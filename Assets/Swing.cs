a62d502facc7490be94fde37bed7678f9a839556
Paul Sinnett <paul.sinnett@gmail.com>
2020-10-13 17:20:13 +0100

    Initial commit

---
+++ Assets/Swing.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swing : MonoBehaviour
+{
+    HingeJoint joint;
+    public Rigidbody dog;
+
+    void Start()
+    {
+//        joint = GetComponent<HingeJoint>();    
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            joint = gameObject.AddComponent<HingeJoint>();
+            dog.transform.position = dog.transform.position + Vector3.up;
+            joint.connectedBody = dog; 
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            Destroy(joint);
+            joint = null;
+        }
+    }
+}
